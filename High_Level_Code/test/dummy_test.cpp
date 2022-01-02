83a06d2c2fefe56fb3f9c5c4487bee3c6df4fe3e
Sahil Kale <32375512+sahil-kale@users.noreply.github.com>
2022-01-02 01:13:29 -0600

    Add in simple testing infrastructure

--- High_Level_Code/test/dummy_test.cpp
+++ High_Level_Code/test/dummy_test.cpp
@@ -1,4 +1,5 @@
 #include "CppUTest/TestHarness.h"
+#include "add.h"
 
 // create a test group
 TEST_GROUP(dummy_test){
@@ -8,5 +9,6 @@ TEST_GROUP(dummy_test){
 //create a test for that test group
 TEST (dummy_test, pass_me){
     CHECK_EQUAL(1,1);
+    CHECK_EQUAL(2, add(1,1));
 }
 
