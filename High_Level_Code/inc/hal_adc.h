8b8d577ce50b88da77965697d74c1d4aa60f4461
Sahil Kale <32375512+sahil-kale@users.noreply.github.com>
2022-01-04 00:37:29 -0600

    edit ADC hal mocks

--- High_Level_Code/inc/hal_adc.h
+++ High_Level_Code/inc/hal_adc.h
@@ -4,6 +4,18 @@
 
 void hal_adc_init(void);
 
-uint16_t hal_adc_get_value(void);
+/**
+ * @brief Returns the ADC value from onboard temperature sensor
+ * 
+ * @return uint16_t 12 bit ADC result
+ */
+uint16_t hal_adc_get_temp_value(void);
+
+/**
+ * @brief Returns the ADC value from onboard voltage divider
+ * 
+ * @return uint16_t 12 bit ADC result
+ */
+uint16_t hal_adc_get_voltage_value(void);
 
 #endif
--- High_Level_Code/test/mocks/mock_hal_adc.c
+++ High_Level_Code/test/mocks/mock_hal_adc.c
@@ -6,7 +6,14 @@ void hal_adc_init(void)
     return;
 }
 
-uint16_t hal_adc_get_value(void)
+uint16_t hal_adc_get_temp_value(void)
+{
+    mock_c()->actualCall("hal_adc_get_value");
+    uint16_t ret = (uint16_t)(mock_c()->returnUnsignedIntValueOrDefault(0));
+    return ret;
+}
+
+uint16_t hal_adc_get_voltage_value(void)
 {
     mock_c()->actualCall("hal_adc_get_value");
     uint16_t ret = (uint16_t)(mock_c()->returnUnsignedIntValueOrDefault(0));
